3d59c34ddbacb897108099a1618e7694e8330446
wira4512 <wbekti30@gmail.com>
2026-08-29 11:40:24 +0700

    git commit -am “week01: refactor to use val and string templates”

--- src/oop_94079_mariamakarioswirabektidamian/week01/GradingSystem.kt
+++ src/oop_94079_mariamakarioswirabektidamian/week01/GradingSystem.kt
@@ -1,8 +1,8 @@
 package oop_94079_mariamakarioswirabektidamian.week01
 
 fun main() {
-    var name: String = "John Thor"
-    var score: Int = 80
+    val name: String = "John Thor"
+    val score: Int = 80
 
-    println("Nama: " + name + ", Nilai: " + score)
+    println("Nama: $name, Nilai: $score")
 }
