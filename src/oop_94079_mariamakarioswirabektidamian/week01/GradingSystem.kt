23e3ab0e3aac80c8de455d26c01fc6cff1f2a656
wira4512 <wbekti30@gmail.com>
2026-08-29 11:37:45 +0700

    git add. git commit -m “week01: initial setup and procedural code”

---
+++ src/oop_94079_mariamakarioswirabektidamian/week01/GradingSystem.kt
@@ -0,0 +1,8 @@
+package oop_94079_mariamakarioswirabektidamian.week01
+
+fun main() {
+    var name: String = "John Thor"
+    var score: Int = 80
+
+    println("Nama: " + name + ", Nilai: " + score)
+}
