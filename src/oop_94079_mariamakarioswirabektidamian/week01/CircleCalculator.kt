fdd691c5dc49cc61faf8a6a9053a0fe4ce344f7b
wira4512 <wbekti30@gmail.com>
2026-09-04 14:37:52 +0700

    week01: (tugas 1) add legacy CircleCalculator code

---
+++ src/oop_94079_mariamakarioswirabektidamian/week01/CircleCalculator.kt
@@ -0,0 +1,20 @@
+package oop_94079_mariamakarioswirabektidamian.week01
+
+fun main(args: Array<String>) {
+    // Variable definition
+    var radius: Double = 7.0
+    var pi: Double = 3.14
+    // Calculation
+    var area: Double = pi * radius * radius
+    // Output Concatenation
+    println("Radius: " + radius + ", Area: " + area)
+    // Logic check
+    checkSize(area)
+}
+fun checkSize(area: Double) {
+    if (area > 100) {
+        println("This is a Big Circle")
+    } else {
+        println("This is a Small Circle")
+    }
+}
