b43e06e6dbca49db4b21fc08c79eab5be6515bf2
wira4512 <wbekti30@gmail.com>
2026-09-04 15:21:26 +0700

    week01: (tugas 2) implement discount logic

--- src/oop_94079_mariamakarioswirabektidamian/week01/GameStore.kt
+++ src/oop_94079_mariamakarioswirabektidamian/week01/GameStore.kt
@@ -3,4 +3,12 @@ package oop_94079_mariamakarioswirabektidamian.week01
 fun main() {
     val gameTitle = "Cyberpunk 2077"
     val price = 600000
+
+    val discount = calculateDiscount(price)
+}
+
+fun calculateDiscount(price: Int) = if (price > 500000) {
+    20
+} else {
+    10
 }
