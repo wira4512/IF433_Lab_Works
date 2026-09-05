dca575f950b5fe362fdeeb165328a122e6d901c3
wira4512 <wbekti30@gmail.com>
2026-09-05 08:25:29 +0700

    "week02: add secondary constructor"

--- src/oop_94079_mariamakarioswirabektidamian/week02/Main.kt
+++ src/oop_94079_mariamakarioswirabektidamian/week02/Main.kt
@@ -16,12 +16,26 @@ fun main() {
     scanner.nextLine()
 
     if (nim.length != 5) {
-        println("ERROR: Pendaftaran dibatalkan. NIM hraus 5 karakter!")
+        println("ERROR: Pendaftaran dibatalkan. NIM harus 5 karakter!")
     } else {
-        print("Masukkan Jurusan: ")
-        val major = scanner.next()
+
+        print("Masukkan Jurusan (Kosongkan jika belum memilih): ")
+        val inputMajor = scanner.nextLine()
+
+        // Default jurusan jika mahasiswa belum memilih jurusan
+        val major: String
+
+        if (inputMajor.isBlank()) {
+            major = "Non-Matriculated"
+        } else {
+            major = inputMajor
+        }
 
         val s1 = Student(name, nim, major)
-        println("Status: Pendaftaran Selesai.")
+
+        println("\nStatus: Pendaftaran Selesai.")
+        println("Nama: ${s1.name}")
+        println("NIM: ${s1.nim}")
+        println("Jurusan: ${s1.major}")
     }
 }
