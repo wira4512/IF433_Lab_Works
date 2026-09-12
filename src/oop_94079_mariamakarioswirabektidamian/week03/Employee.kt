c533b56623de7a51e693b2e83e8981263cc8e3fd
wira4512 <wbekti30@gmail.com>
2026-09-12 10:10:49 +0700

    week03: cleanup code

--- src/oop_94079_mariamakarioswirabektidamian/week03/Employee.kt
+++ src/oop_94079_mariamakarioswirabektidamian/week03/Employee.kt
@@ -10,7 +10,7 @@ class Employee(val name: String) {
                 field = value
             }
         }
-    // Hanya bisa diakses di file/class ini saja
+
     private var performanceRating: Int = 3
 
     fun increasePerformance() {
@@ -18,12 +18,10 @@ class Employee(val name: String) {
         println("Kinerja $name meningkat! Rating: $performanceRating")
     }
 
-    // Kita tidak buat getter untuk performanceRating, jadi data ini benar-benar rahasia
-    // kecuali kita buat function khusus untuk print.
     fun printStatus() {
         println("Karyawan: $name, Rating: $performanceRating")
     }
-    // Tax (Pajak) dihitung 10% dari gaji saat ini
+
     val tax: Double
         get() = salary*0.1
 }
