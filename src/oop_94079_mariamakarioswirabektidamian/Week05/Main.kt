9e9310516289f5d0fef41093ba6eba18b5defe88
wira4512 <wbekti30@gmail.com>
2026-09-26 09:43:36 +0700

    week05: (task 1) test MathHelper overloading in main

--- src/oop_94079_mariamakarioswirabektidamian/Week05/Main.kt
+++ src/oop_94079_mariamakarioswirabektidamian/Week05/Main.kt
@@ -1,27 +1,18 @@
 package oop_94079_mariamakarioswirabektidamian.Week05
 
 fun main() {
-        val dosen1 = Dosen(nama = "Pak Alex", nidn = "0123456")
-        val admin1 = Admin(nama = "Bu Siti")
 
-        val daftarPegawai: List<Pegawai> = listOf(dosen1, admin1)
+    val mathHelper = MathHelper()
 
-        println("=== AKTIVITAS PEGAWAI ===")
-        for (pegawai in daftarPegawai) {
+    // Luas persegi
+    val luasPersegi = mathHelper.hitungLuas(5)
+    println("Luas persegi = $luasPersegi")
 
-            pegawai.bekerja()
+    // Luas persegi panjang
+    val luasPersegiPanjang = mathHelper.hitungLuas(10, 5)
+    println("Luas persegi panjang = $luasPersegiPanjang")
 
-
-            when (pegawai) {
-                is Dosen -> {
-                    println("=> Terdeteksi sebagai Dosen (NIDN: ${pegawai.nidn})")
-                    pegawai.mengajar()
-                }
-                is Admin -> {
-                    println("=> Terdeteksi sebagai Admin")
-                    pegawai.doAdminWork()
-                }
-            }
-            println("-----------------")
-        }
-    }
+    // Luas lingkaran
+    val luasLingkaran = mathHelper.hitungLuas(7.0)
+    println("Luas lingkaran = $luasLingkaran")
+}
